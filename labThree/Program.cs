25ab99e1266cb6ba84d77bc1a8645b9e15b4853a
bjm37 <bjm37@calvin.edu>
2017-09-16 15:17:21 -0400

    First draft

--- labThree/Program.cs
+++ labThree/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace labThree
 {
@@ -10,6 +6,35 @@ namespace labThree
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Learn your squares and cubes! Enter an integer:");
+            int input = int.Parse(Console.ReadLine());
+
+        }
+        public static bool Continue()
+        {
+            Console.WriteLine("Do you wish to Continue? y/n");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "y")
+            {
+                goOn = true;
+            }
+            else if (input == "n")
+            {
+                goOn = false;
+            }
+            else
+            {
+                //So this is a neat trick call recursion, it involves calling a method inside a method
+                //In a sense it's another way of doing a loop, but using methods 
+                //it works in much the same way, it's divided into two parts: 
+                //a step and an end condition. 
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = Continue();
+            }
+
+            return goOn;
         }
     }
 }
